29fc9f75f31183c6a98db58e59af6aa899f8fcbe
shihan <shihananuruddha@gmail.com>
2017-10-04 10:44:40 +0530

    done

--- src/cropper/NewClass.java
+++ src/cropper/NewClass.java
@@ -1,21 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package cropper;
 
-import static com.idrsolutions.pdf.acroforms.xfa.XScriptNode.page;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import org.jpedal.examples.images.ConvertPagesToImages;
 import org.jpedal.exception.PdfException;
-import static org.omg.IOP.IORHelper.extract;
 
-/**
- *
- * @author shihan
- */
 public class NewClass {
     
     
